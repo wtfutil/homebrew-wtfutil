8e97b14d93fdb2588ad68545d2442c99a2ca1b39
goreleaserbot <goreleaser@carlosbecker.com>
2022-10-09 09:28:36 -0700

    Brew formula update for wtf version v0.42.0

--- wtfutil.rb
+++ wtfutil.rb
@@ -5,36 +5,51 @@
 class Wtfutil < Formula
   desc "The personal information dashboard for your terminal."
   homepage "https://wtfutil.com"
-  version "0.41.0"
-  bottle :unneeded
+  version "0.42.0"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/wtfutil/wtf/releases/download/v0.41.0/wtf_0.41.0_darwin_amd64.tar.gz"
-      sha256 "04916a746c3fa77f5b9d153e62b0db79334501747b8d3476e3e61cfcf32a723b"
-    end
     if Hardware::CPU.arm?
-      url "https://github.com/wtfutil/wtf/releases/download/v0.41.0/wtf_0.41.0_darwin_arm64.tar.gz"
-      sha256 "2d0f58606a1fadcc6444bf98a2573bbcc3f5686f0bbf26ed10635defecba2f60"
+      url "https://github.com/wtfutil/wtf/releases/download/v0.42.0/wtf_0.42.0_darwin_arm64.tar.gz"
+      sha256 "2c317145ec5b4c84fd8ec45c177a14ec41eff6db1eb4cf84f6228cd38e4229e0"
+
+      def install
+        bin.install "wtfutil"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/wtfutil/wtf/releases/download/v0.42.0/wtf_0.42.0_darwin_amd64.tar.gz"
+      sha256 "2249c054711fc04a84a8773f9bedb3d75d9060ab20e2776101d548813189af78"
+
+      def install
+        bin.install "wtfutil"
+      end
     end
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/wtfutil/wtf/releases/download/v0.41.0/wtf_0.41.0_linux_amd64.tar.gz"
-      sha256 "409f686cd151ba416ea3bb9c9d45ba39d62b3a1a70c706a17734175edbef63fb"
-    end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/wtfutil/wtf/releases/download/v0.41.0/wtf_0.41.0_linux_armv6.tar.gz"
-      sha256 "df481f864d47002e00a9278b0092fd5780d39e2ab356f58f03e977ae517ecb92"
+      url "https://github.com/wtfutil/wtf/releases/download/v0.42.0/wtf_0.42.0_linux_armv6.tar.gz"
+      sha256 "1fa0f6bcdb97c780a6528d70a933a867e892c0d09a0da8df4d74438b796a4ccb"
+
+      def install
+        bin.install "wtfutil"
+      end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/wtfutil/wtf/releases/download/v0.41.0/wtf_0.41.0_linux_arm64.tar.gz"
-      sha256 "49731fd061645ee6be0b7ae8b6207a2ef2213add10dcb410ff7c770cca02163b"
-    end
-  end
+      url "https://github.com/wtfutil/wtf/releases/download/v0.42.0/wtf_0.42.0_linux_arm64.tar.gz"
+      sha256 "28b5d94805edaa07212f8614f50392dbbe438bb7d07b861d310fd3b403fe2673"
 
-  def install
-    bin.install "wtf"
+      def install
+        bin.install "wtfutil"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/wtfutil/wtf/releases/download/v0.42.0/wtf_0.42.0_linux_amd64.tar.gz"
+      sha256 "940fe329915bdebbed4b3496e046fac045e10414fbf5bc7c5c4dcf187f8582ed"
+
+      def install
+        bin.install "wtfutil"
+      end
+    end
   end
 end
